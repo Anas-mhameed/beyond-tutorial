c3a865381b1158468b8ba6536c372c9e4747d542
Anas-Mhameed <mhameedanaskhader@Gmail.com>
2022-10-26 20:43:09 +0300

    Adding read-only view function and templates
    Adding all the files we need inorder to make the project run including views.py & template dir.

---
+++ msgboard/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import Message
+from .forms import MessageForm
+
+# Create your views here.
+
+
+def board(request):
+    messages = Message.objects.order_by('-date')
+    #if request.method == "POST":
+    #    form = MessageForm(request.POST)
+    #    if form.is_valid():
+    #        form.save()
+    #        return redirect('board')
+    #else:
+    #    form = MessageForm() = > 'form': form, 
+    return render(request, 'msgboard/board.html', {'messages': messages, })
